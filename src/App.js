cd9329b2d531fbe1cae52de41aa34c9254102830
Maysam Ab <maysam82.dev@gmail.com>
2020-08-06 11:58:40 +0430

    Created Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import classes from './app.module.scss';
+import Header from './components/Header/Header';
 
 const App = () => {
-	return <div className={classes.appContainer}>App</div>;
+	return (
+		<div className={classes.appContainer}>
+			<Header />
+		</div>
+	);
 };
 
 export default App;
