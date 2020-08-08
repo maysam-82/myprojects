ba0a9f31db79637818c4d164b8183822ae4fb50a
Maysam Ab <maysam82.dev@gmail.com>
2020-08-08 11:56:05 +0430

    Reordered data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,20 @@
+const techList = {
+	REACT: { techId: '101', name: 'React' },
+	TYPESCRIPT: { techId: '102', name: 'Typescript' },
+	REDUX: { techId: '103', name: 'redux' },
+	REDUX_THUNK: { techId: '104', name: 'redux-thunk' },
+	REACT_BOOTSTRAP: { techId: '105', name: 'react-bootstrap' },
+	REACT_HOOKS: { techId: '106', name: 'React/Hooks' },
+	SEMANTIC_UI: { techId: '107', name: 'semantic-ui' },
+	REACT_ROUTER: { techId: '108', name: 'react-router-dom' },
+	REACT_STORY_BOOK: { techId: '109', name: 'storybook' },
+};
+
+const testLibraryList = {
+	JEST: { testId: '201', testLibraryName: 'jest' },
+	ENZYME: { testId: '202', testLibraryName: 'enzyme' },
+	REACT_TEST_RENDERER: { testId: '203', testLibraryName: 'react-test-renderer' },
+};
 export const projects = [
 	{
 		id: 1,
@@ -44,21 +61,3 @@ export const projects = [
 		technologies: [techList.REACT, techList.REACT_STORY_BOOK],
 	},
 ];
-
-const techList = {
-	REACT: { techId: '101', name: 'React' },
-	TYPESCRIPT: { techId: '102', name: 'Typescript' },
-	REDUX: { techId: '103', name: 'redux' },
-	REDUX_THUNK: { techId: '104', name: 'redux-thunk' },
-	REACT_BOOTSTRAP: { techId: '105', name: 'react-bootstrap' },
-	REACT_HOOKS: { techId: '106', name: 'React/Hooks' },
-	SEMANTIC_UI: { techId: '107', name: 'semantic-ui' },
-	REACT_ROUTER: { techId: '108', name: 'react-router-dom' },
-	REACT_STORY_BOOK: { techId: '109', name: 'storybook' },
-};
-
-const testLibraryList = {
-	JEST: { testId: '201', testLibraryName: 'jest' },
-	ENZYME: { testId: '202', testLibraryName: 'enzyme' },
-	REACT_TEST_RENDERER: { testId: '203', testLibraryName: 'react-test-renderer' },
-};
