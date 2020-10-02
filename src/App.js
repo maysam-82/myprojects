fbda72d7640d750615e132aed76df8b80736d8f1
maysam <maysam82.dev@gmail.com>
2020-10-02 23:53:45 +1000

    Add mterial ui to Footer component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ import theme from './styles/theme';
 import { ThemeProvider, Container, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    appContainer: {
+    appTopSpace: {
         ...theme.mixins.toolbar,
         marginBottom: '0.625rem',
+    },
+    appBottomSpace: {
+        height: '4rem',
+        [theme.breakpoints.up('md')]: {
+            height: 0,
+        },
     },
 }));
 
@@ -19,16 +25,16 @@ const App = () => {
     return (
         <ThemeProvider theme={theme}>
             <Header />
-            <div className={classes.appContainer} />
+            <div className={classes.appTopSpace} />
             <Container maxWidth="lg">
-                <div className={classes.appContent} />
-                <div>
+                <div className={classes.appContent}>
                     <HeroBox />
                     {/*TODO: Complete Slider <Slider items={projects} /> */}
                     <Board items={projects} />
                 </div>
-                <Footer items={projects} />
             </Container>
+            <div className={classes.appBottomSpace} />
+            <Footer items={projects} />
         </ThemeProvider>
     );
 };
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,134 @@
+import {
+    Chip,
+    Grid,
+    Link,
+    List,
+    ListItem,
+    makeStyles,
+} from '@material-ui/core';
+import FaceIcon from '@material-ui/icons/Face';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import React from 'react';
-import classes from './footer.module.scss';
+
+const useStyles = makeStyles((theme) => {
+    const {
+        palette: {
+            common: { secondary, white },
+        },
+        typography,
+    } = theme;
+
+    return {
+        footerContainer: {
+            padding: '1rem',
+            backgroundColor: secondary,
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            position: 'fixed',
+            top: 'auto',
+            bottom: 0,
+
+            [theme.breakpoints.up('md')]: {
+                padding: 0,
+                position: 'relative',
+            },
+        },
+        projectLinksContainer: {
+            display: 'none',
+            border: 'none',
+            [theme.breakpoints.up('md')]: {
+                display: 'block',
+                borderLeft: '0.0625rem solid',
+                borderLeftColor: white,
+            },
+        },
+        listItemGithub: {
+            color: secondary,
+            fontSize: '1.5rem',
+            backgroundColor: white,
+        },
+        profile: {
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center',
+            fontSize: typography.h5.fontSize,
+            color: white,
+        },
+        projectsLink: {
+            display: 'none',
+
+            [theme.breakpoints.up('md')]: {
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'space-between',
+                '&::after': {
+                    content: "''",
+                    flex: 'auto',
+                    width: 'calc(20% - 0.625rem)',
+                },
+            },
+        },
+        projectLink: {
+            padding: '0.625rem',
+            width: 'auto',
+            color: white,
+        },
+        iconGithub: {
+            color: secondary,
+        },
+        iconLink: {
+            color: white,
+        },
+        listItemLink: {
+            color: white,
+            borderColor: white,
+        },
+    };
+});
 
 const Footer = ({ items }) => {
-	const renderItemsLink = items.map(({ id, title, url }) => (
-		<li className={classes.projectLink} key={id}>
-			<a href={url}>{title}</a>
-		</li>
-	));
-	return (
-		<div className={classes.footerContainer}>
-			<div className={classes.profile}>
-				<a href="https://github.com/maysam-82">My Github Profile</a>
-			</div>
-			<ul className={classes.projectsLink}>{renderItemsLink}</ul>
-		</div>
-	);
+    const classes = useStyles();
+
+    const renderItemsLink = items.map(({ id, title, url }) => (
+        <ListItem
+            className={classes.projectLink}
+            key={id}
+            component={Link}
+            href={url}
+        >
+            <Chip
+                label={title}
+                icon={<CheckCircleIcon className={classes.iconLink} />}
+                clickable
+                variant="outlined"
+                className={classes.listItemLink}
+            />
+        </ListItem>
+    ));
+    return (
+        <footer className={classes.footerContainer}>
+            <Grid item md={3} xs={12}>
+                <Link
+                    href="https://github.com/maysam-82"
+                    className={classes.profile}
+                    underline="none"
+                >
+                    <Chip
+                        label="My Github"
+                        icon={<FaceIcon className={classes.iconGithub} />}
+                        clickable
+                        className={classes.listItemGithub}
+                    />
+                </Link>
+            </Grid>
+            <Grid item md={9} className={classes.projectLinksContainer}>
+                <List className={classes.projectsLink}>{renderItemsLink}</List>
+            </Grid>
+        </footer>
+    );
 };
 
 export default Footer;
