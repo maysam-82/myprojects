6d50a1c57ba08932ac40a086a279f3932de71db6
maysam <maysam82.dev@gmail.com>
2020-10-03 12:39:56 +1000

    Refactor Board with material-ui

--- src/components/Board/BoardItem/BoardItem.js
+++ src/components/Board/BoardItem/BoardItem.js
@@ -1,26 +1,123 @@
+import {
+    Card,
+    CardActions,
+    CardContent,
+    Grid,
+    Link,
+    List,
+    ListItem,
+    makeStyles,
+    Typography,
+} from '@material-ui/core';
 import React from 'react';
-import classes from './boardItem.module.scss';
+
+const useStyles = makeStyles(
+    ({
+        palette: {
+            common: { red, black },
+        },
+        border,
+    }) => ({
+        boardItem: {
+            width: 'calc(50% - 0.3125rem)',
+            marginBottom: '0.625rem',
+        },
+        boardCard: {
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        label: {
+            color: red,
+            opacity: 0.8,
+            margin: 0,
+            border,
+        },
+        title: {
+            color: black,
+            fontWeight: 700,
+            padding: '0.625rem 0',
+            marginBottom: '0.625rem',
+            borderBottom: border.border,
+            borderBottomColor: border.borderColor,
+        },
+        urlContainer: {
+            borderTop: border.border,
+            borderTopColor: border.borderColor,
+            marginTop: 'auto',
+            margin: '0 1rem',
+            padding: '0.625rem 0',
+        },
+        urlLinkButton: {
+            textTransform: 'lowercase',
+            '&:hover': {
+                textDecoration: 'none',
+                backgroundColor: 'transparent',
+            },
+        },
+        boardCardContent: {
+            marginBottom: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+        },
+        item: {
+            height: '100%',
+        },
+    })
+);
 
 const BoardItem = ({ item }) => {
     const { title, url, technologies } = item || {};
+    const classes = useStyles();
 
     const renderTechnologies = technologies.map(({ techId, name }) => (
-        <li className={classes.item} key={techId}>
+        <ListItem divider key={techId}>
             {name}
-        </li>
+        </ListItem>
     ));
 
     return (
-        <li className={classes.boardItem} key={item.id}>
-            <div className={classes.itemTitle}>Project Title:</div>
-            <h4 className={classes.itemTitleDescription}>{title}</h4>
-            <div className={classes.itemTitle}>Technologies:</div>
-            <ul className={classes.items}>{renderTechnologies}</ul>
-            <div className={classes.urlContainer}>
-                <div className={classes.urlLabel}>URL:</div>
-                <a href={url}>{url}</a>
-            </div>
-        </li>
+        <Grid item xs={12} md={6} lg={4}>
+            <ListItem className={classes.item}>
+                <Card className={classes.boardCard}>
+                    <CardContent className={classes.boardCardContent}>
+                        <Typography
+                            className={classes.label}
+                            color="textSecondary"
+                            gutterBottom
+                        >
+                            Project title:
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            component="p"
+                            className={classes.title}
+                        >
+                            {title}
+                        </Typography>
+
+                        <Typography
+                            className={classes.label}
+                            color="textSecondary"
+                        >
+                            Technologies:
+                        </Typography>
+                        <List>{renderTechnologies}</List>
+                    </CardContent>
+                    <CardActions className={classes.urlContainer}>
+                        <Link
+                            href={url}
+                            className={classes.urlLinkButton}
+                            color="secondary"
+                        >
+                            {url}
+                        </Link>
+                    </CardActions>
+                </Card>
+            </ListItem>
+        </Grid>
     );
 };
 
