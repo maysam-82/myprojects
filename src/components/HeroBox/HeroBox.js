08bcde0ee72336b40b482499bdfd975abb22ed1a
maysam <maysam82.dev@gmail.com>
2020-10-03 14:25:03 +1000

    Refactor Herobox with Material-ui

--- src/components/HeroBox/HeroBox.js
+++ src/components/HeroBox/HeroBox.js
@@ -1,12 +1,49 @@
+import { makeStyles, Paper, Typography } from '@material-ui/core';
+import heroImage from '../../assets/img/hero.png';
 import React from 'react';
-import classes from './heroBox.module.scss';
 
-const HeroBox = ({ items }) => {
-	return (
-		<div className={classes.heroBoxContainer}>
-			<h1>Welcome to Maysam Abolghasemi's sample projects</h1>
-		</div>
-	);
+const useStyles = makeStyles(
+    ({
+        palette: {
+            common: { black, body },
+        },
+        shape: { borderRadius },
+        breakpoints,
+    }) => ({
+        heroBoxContainer: {
+            backgroundImage: `url(${heroImage})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            height: '25rem',
+            display: 'none',
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: '0 1rem',
+            [breakpoints.up('md')]: {
+                display: 'flex',
+            },
+        },
+        heroBoxTitle: {
+            padding: '0.625rem',
+            color: black,
+            fontWeight: 700,
+            backgroundColor: body,
+            opacity: 0.6,
+            borderRadius: borderRadius,
+        },
+    })
+);
+
+const HeroBox = () => {
+    const classes = useStyles();
+    return (
+        <Paper className={classes.heroBoxContainer}>
+            <Typography variant="h4" className={classes.heroBoxTitle}>
+                Welcome to Maysam Abolghasemi's Dev. Website
+            </Typography>
+        </Paper>
+    );
 };
 
 export default HeroBox;
