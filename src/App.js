1e91e2011b068d3cf5032f82f47929ced35a98fe
maysam <maysam82.dev@gmail.com>
2020-10-15 00:02:43 +1100

    Add ThemeSwitch to App-Add dark theme state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import HeroBox from './components/HeroBox';
 import { projects } from './data/projects';
 import Footer from './components/Footer';
 import Board from './components/Board';
-import theme from './styles/theme';
 import { ThemeProvider, Container, makeStyles } from '@material-ui/core';
+import { setTheme } from './utils/theme';
+import ThemeSwitch from './components/ThemeSwitch/ThemeSwitch';
 
 const useStyles = makeStyles((theme) => ({
     appTopSpace: {
@@ -21,15 +22,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const App = () => {
+    const [isDarkMode, setDarkMode] = useState(false);
+
     const classes = useStyles();
     return (
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={setTheme(isDarkMode)}>
             <Header />
             <div className={classes.appTopSpace} />
+            <ThemeSwitch
+                isDark={isDarkMode}
+                changeTheme={() => setDarkMode(!isDarkMode)}
+            />
             <Container maxWidth="lg">
                 <div className={classes.appContent}>
                     <HeroBox />
-                    {/*TODO: Complete Slider <Slider items={projects} /> */}
                     <Board items={projects} />
                 </div>
             </Container>
